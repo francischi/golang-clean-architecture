a90005a78dbd1341db678f3dfe9a9794986d6592
francischi <francischiooo@gmail.com>
2023-05-15 00:08:46 +0800

    2023/5/15 add memberService unittest logIn function

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/google/wire v0.5.0
 	github.com/joho/godotenv v1.5.1
@@ -37,9 +38,9 @@ require (
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/member/login_test.go
@@ -0,0 +1,58 @@
+package member
+
+// mockgen -source="./pkg/repos/interfaces/MemberInterface.go" -destination="./test/member/MemberRepo.go"
+
+import (
+	// "fmt"
+    "testing"
+	"golang/pkg/memberModule"
+	"golang/pkg/memberModule/dtos"
+	"golang/pkg/repos/models"
+	"github.com/golang/mock/gomock"
+)
+
+func createRepoReturn()(member models.MemberModel){
+    var model models.MemberModel
+    model.Id = 1
+    model.Email = "test@gmail.com"
+    model.MemberId = "d69b16e9-61ee-46d1-a99e-894b760fa5a3"
+    model.Name = "frank"
+    model.CreateTime = 100
+    model.DeleteTime = 100
+    model.IsOnline = 0
+    model.Password = "4cb6390de5467430a51932c0b87e8f1ec7ec33fbf08486f77704a6cd6f89c5de"
+    
+    return model
+}
+
+func createLogInDto()(login dtos.LogInDto){
+    var dto dtos.LogInDto
+    dto.Account = "test@gmail.com"
+    dto.Password = "thisisfrank4"
+    return dto
+}
+
+func prepareMockRepo(controller *gomock.Controller) *MockMemberRepo {
+    memberRepo := NewMockMemberRepo(controller)
+    email := "test@gmail.com"
+    memberModel  := createRepoReturn()
+    memberRepo.EXPECT().GetMemberByEmail(email).Return(memberModel , nil)
+    return memberRepo
+}
+
+func TestMemberServiceLogIn(t *testing.T){
+    controller := gomock.NewController(t)
+    defer controller.Finish()
+
+    memberRepo := prepareMockRepo(controller)
+
+    memberService := memberModule.MemberService{
+        MemberRepo: memberRepo,
+    }
+
+    logInDto := createLogInDto()
+    token , err := memberService.LogIn(&logInDto)
+    if token =="" || err!=nil{
+        t.Errorf("errMessage:%s",err.Error())
+    }
+}
