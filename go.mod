382ecae22a545fa33a8edef9db83013a73b7c252
francischi <francischiooo@gmail.com>
2023-05-17 13:46:41 +0800

    2023/5/17 adjust unitTest member create_test

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/google/wire v0.5.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.1
 	gorm.io/driver/mysql v1.5.0
 	gorm.io/gorm v1.25.0
 )
@@ -16,6 +17,7 @@ require (
 require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -34,6 +36,7 @@ require (
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.27.6 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
--- unitTest/test/member/create_test.go
+++ unitTest/test/member/create_test.go
@@ -7,31 +7,17 @@ import (
 	"golang/pkg/repos/models"
 	"golang/unitTest/mockRepos"
 	"testing"
-
+	"github.com/stretchr/testify/assert"
 	"github.com/golang/mock/gomock"
 )
-
-func createDto()(dtos.CreateMemberDto){
-	var dto dtos.CreateMemberDto
-	dto.Name = "frank"
-	dto.Email = "test@gmail.com"
-	dto.Gender = "FEMALE"
-	dto.Password = "thisisfrank"
-	return dto
-}
-
-func createInput()(member models.MemberModel){
-	var model models.MemberModel
-	return model
-}
 
 func TestCreate(t *testing.T){
 	controller := gomock.NewController(t)
     defer controller.Finish()
 	
 	memberRepo := mockRepos.NewMockMemberRepo(controller)
-	
-	memberRepo.EXPECT().IsEmailExist(gomock.Any()).Return(false)
+
+	memberRepo.EXPECT().IsEmailExist(gomock.Any()).Return(false).AnyTimes()
 
 	memberRepo.EXPECT().Create(gomock.Any()).DoAndReturn(
 		func(input models.MemberModel) interface{}{
@@ -41,16 +27,67 @@ func TestCreate(t *testing.T){
 			}
 			return nil
 		},
-	)
-
+	).AnyTimes()
+	
 	memberService := memberModule.MemberService{
-        MemberRepo: memberRepo,
-    }
-
-	createDto := createDto()
-
-	err := memberService.Create(&createDto)
-	if err!= nil{
-		t.Errorf("errMessage:%s",err.Error())
+		MemberRepo: memberRepo,
 	}
+
+	t.Run("success", func(t *testing.T) {
+		createDto := dtos.CreateMemberDto{
+			Name : "frank",
+			Email : "test@gmail.com",
+			Gender : "FEMALE",
+			Password : "thisisfrank",
+		}
+
+		err := memberService.Create(&createDto)
+		assert.NoError(t, err)
+	})
+
+	t.Run("invalid name", func(t *testing.T) {
+
+		createDto := dtos.CreateMemberDto{
+			Email : "test@gmail.com",
+			Gender : "FEMALE",
+			Password : "thisisfrank",
+		}
+
+		err := memberService.Create(&createDto)
+		assert.Error(t, err)
+	})
+
+	t.Run("required email", func(t *testing.T) {
+		createDto := dtos.CreateMemberDto{
+			Name : "frank",
+			Gender : "FEMALE",
+			Password : "thisisfrank",
+		}
+
+		err := memberService.Create(&createDto)
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid email", func(t *testing.T) {
+		createDto := dtos.CreateMemberDto{
+			Name : "frank",
+			Email : "test.com",
+			Gender : "FEMALE",
+			Password : "thisisfrank",
+		}
+
+		err := memberService.Create(&createDto)
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid password", func(t *testing.T) {
+		createDto := dtos.CreateMemberDto{
+			Name : "frank",
+			Email : "test@gmail.com",
+			Password : "thisisfrank",
+		}
+
+		err := memberService.Create(&createDto)
+		assert.Error(t, err)
+	})
 }
