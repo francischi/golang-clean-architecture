7de89fd408a1f5448274208a97e5063b8e1e8c47
francischi <francischiooo@gmail.com>
2023-05-24 17:05:56 +0800

    2023/5/24 adjust memberRoutes

--- pkg/memberModule/MemberRoutes.go
+++ pkg/memberModule/MemberRoutes.go
@@ -8,7 +8,14 @@ import (
 func SetRoute(g *gin.Engine ,baseGroup string){
 
 	memberGroup :=g.Group(baseGroup+"/member")
-	memberGroup.POST("" , InitMemberController().Create)
-	memberGroup.POST("/login" , InitMemberController().LogIn)
-	memberGroup.PATCH("/password" , mw.InitJwtMiddleWare().ConfirmToken , InitMemberController().ChangePwd)
+	memberGroup.POST("" , 
+		func(ctx *gin.Context){InitMemberController().Create(ctx)},
+	)
+	memberGroup.POST("/login" ,
+		func(ctx *gin.Context){InitMemberController().LogIn(ctx)},
+	)
+	memberGroup.PATCH("/password" , 
+		func(ctx *gin.Context) {mw.InitJwtMiddleWare().ConfirmToken(ctx)} , 
+		func(ctx *gin.Context) {InitMemberController().ChangePwd(ctx)},
+	)
 }
